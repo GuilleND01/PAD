1b3d3d996dc342eb9e01f1b86e02af77f096a3c7
imclau22 <claudi-4@hotmail.es>
2023-11-03 15:50:03 +0100

    Conexion con la BD
    Co-Authored-By: jbroto <126669094+jbroto@users.noreply.github.com>

--- ReadCycle/app/src/main/java/es/ucm/fdi/readcycle/presentacion/MainActivity.java
+++ ReadCycle/app/src/main/java/es/ucm/fdi/readcycle/presentacion/MainActivity.java
@@ -1,8 +1,18 @@
 package es.ucm.fdi.readcycle.presentacion;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import es.ucm.fdi.readcycle.R;
 
@@ -11,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //conexion con la base de datos//////////////////
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //prueba de lectura
+        db.collection("Usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
         setContentView(R.layout.activity_main);
+
     }
+
 }
