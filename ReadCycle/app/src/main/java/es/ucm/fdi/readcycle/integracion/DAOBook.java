e6ece0a313e830777c0e547b4a88607ee8960bd9
joorgema <86876791+JrgeMA@users.noreply.github.com>
2023-11-27 20:51:28 +0100

    Update en BD DaoBook

--- ReadCycle/app/src/main/java/es/ucm/fdi/readcycle/integracion/DAOBook.java
+++ ReadCycle/app/src/main/java/es/ucm/fdi/readcycle/integracion/DAOBook.java
@@ -4,12 +4,14 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.StorageReference;
@@ -25,6 +27,7 @@ import es.ucm.fdi.readcycle.negocio.BookInfo;
 public class DAOBook {
 
     private final String COL_LIBROS = "Libros";
+    private final String COL_USUARIOS = "Usuarios";
     // Campos de la BD:
     private final String AUTOR = "Autor";
     private final String DESC = "Descripcion";
@@ -41,16 +44,20 @@ public class DAOBook {
 
     public boolean guardarLibro (BookInfo libro) {
 
+        boolean exito = true;
         try {
-            // Id provisonal hay que pensar cuál dar
+
             FirebaseImageStorage imageStorage = new FirebaseImageStorage();
             CollectionReference booksCollection = SingletonDataBase.getInstance().getDB()
                     .collection(COL_LIBROS);
 
+            CollectionReference usersCollection = SingletonDataBase.getInstance().getDB()
+                    .collection(COL_USUARIOS);
+
             FirebaseAuth mAuth = FirebaseAuth.getInstance();
             FirebaseUser currentUser = mAuth.getCurrentUser();
 
-            String id = String.format("%s-%s", libro.getTitle(), libro.getAuthor());
+            String id = String.format("%s-%s-%s", libro.getTitle(), libro.getAuthor(), currentUser.getEmail().toString()).replace("", "");
             Map<String, Object> data = new HashMap<>();
             data.put(AUTOR, libro.getAuthor());
             data.put(TITULO, libro.getTitle());
@@ -60,26 +67,39 @@ public class DAOBook {
             data.put(GENERO, libro.getGenre());
             data.put(PROPIETARIO, currentUser.getEmail());
 
+            /* Se crea la referencia a la imagene en la base de datos*/
             StorageReference fileReference = imageStorage.getStorageRef().child(id + ".png");
 
+            // TODO GESTIONAR EXEPCIONES DE TODO ESTO PROCESO
+            /* Se rellena esa referencia con la imagen*/
             fileReference.putFile(libro.getSelectedImage())
                     .addOnSuccessListener(taskSnapshot -> {
 
+                        /* Una vez guardada la imagen, se tiene el atributo "uri" que contiene la ruta absoluta a la imagen.
+                        * Ya conseguido este atributo, ahora se procede a guardar el libro. */
                         fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
 
                             /* Esta es una URL HTTP que se puede utilizar directamente en un
                             navegador web o con bibliotecas de carga de imágenes (como Glide o
                             Picasso en Android) para recuperar y mostrar la imagen. */
-                            String downloadUrl = uri.toString();
-                            data.put(RUTA_IMAGEN, downloadUrl);
+                            data.put(RUTA_IMAGEN, uri.toString());
 
-                            Task<Void> updateTask = booksCollection.
-                                    document(currentUser.getEmail()).collection("Libros").
-                                    document(id).set(data);
+                            /* Primero se añade el libro a una colección general de ellos.*/
+                            Task<Void> booksCollectionTask = booksCollection.add(data).continueWithTask(new Continuation<DocumentReference, Task<Void>>() {
+
+                                /* Seguido se añade el identificador del libro al array de libros del propio usuario */
+                                @Override
+                                public Task<Void> then(@NonNull Task<DocumentReference> task) throws Exception {
+                                    String nuevoLibroId = task.getResult().getId();
+                                    return usersCollection.document(currentUser.getUid()).update("ID_Libros", FieldValue.arrayUnion(nuevoLibroId));
+
+                                }
+                            });
 
                         });
 
                     });
+
             return true;
         } catch (Exception e){
             return false;
